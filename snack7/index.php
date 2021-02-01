fb103394d6fdbf66512d46f27c27c8aef15cff81
Stephanie <stephanie.furlan94@gmail.com>
2021-02-01 12:49:46 +0100

    Snack 7

---
+++ snack7/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <?php 
+            include 'db_hotel.php';
+            foreach($hotels as $hotel) { ?>
+                <h1><?php echo $hotel["name"]?></h1>
+                <p><?php echo "<b>Description:</b> " .  $hotel["description"]?></p>
+                <p><?php echo "<b>Parking:</b> " . $hotel["parking"]?></p>
+                <p><?php echo "<b>Vote:</b> " . $hotel["vote"]?></p>
+                <p><?php echo "<b>Distance to center:</b> " . $hotel["distance_to_center"]?></p>
+            <?php
+            }
+        ?>
+    </body>
+</html>
---
+++ snack7/main.php
@@ -0,0 +1,5 @@
+<?php 
+
+    include 'db_hotel.php';
+    $a = 3;
+?>
